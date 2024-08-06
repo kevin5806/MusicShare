766426f390ac46daae6ae2a77f5a4f5080e14b72
kevin5806 <kevinleoni58@gmail.com>
2024-08-06 17:16:19 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
                 port: "",
                 pathname: "/**",
             },
+            {
+                protocol: "https",
+                hostname: "platform-lookaside.fbsbx.com",
+                port: "",
+                pathname: "/**",
+            },
         ],
     },
 };
